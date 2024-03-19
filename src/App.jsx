28c8780532a85d0eb4cb2de973f384ffae994bb5
Guilherme Kunsch <suporte@iconexlog.com.br>
2024-03-19 15:59:51 -0300

    adicionando responsividade

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,11 @@ export function App() {
       <Header />
       <div className="flex justify-center items-center flex-wrap">
         <Card
-          className="w-[600px] h-full"
+          className="w-80 md:w-[37.5rem] h-full"
           src="https://api.thingspeak.com/channels/2468578/charts/1?title=Umidade&width=600&type=spline&dynamic=true&results=20"
         />
         <Card
-          className="w-[600px] h-full"
+          className="w-80 md:w-[37.5rem] h-full"
           src="https://api.thingspeak.com/channels/2468578/charts/2?title=Temperatura&width=600&type=spline&dynamic=true&results=20"
         />
       </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 export function Card(props) {
     return (
-      <div className="flex h-96 m-8 mx-auto">
+      <div className="flex h-96 m-8 mx-auto md:flex">
           <iframe className={props.className} src={props.src}></iframe>
       </div>
     );
