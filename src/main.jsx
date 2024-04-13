ac837d4115296ffc59110d20e33fc80d9f8a5b71
Arthur <arthurloyolasoares@gmail.com>
2024-04-13 11:25:48 -0300

    Exclusão de Componentes

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ThemeProvider } from './components/ThemeContext'
 import { Background } from './components/Background'
 import { App } from './App'
 import './global.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider>
         <Background>
           <App />
         </Background>
-    </ThemeProvider>
   </React.StrictMode>,
 )
