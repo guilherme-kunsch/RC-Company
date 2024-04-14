f21977bf6fbb5dc78d63a219c98c31032f7f33f4
guilherme-kunsch <guilherme.kunsch@outlook.com>
2024-04-14 18:36:35 -0300

    fix: vaaaai

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,4 @@
-// import { Logo } from "../components/Logo";
+import { Logo } from "../components/Logo";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
