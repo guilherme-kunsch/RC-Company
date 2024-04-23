9d6fa1c3708258eeb4eb2fb8450624eb3f390e2b
guilherme-kunsch <guilherme.kunsch@outlook.com>
2024-04-22 22:33:05 -0300

    add: criando gráfico

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,14 +1,67 @@
 import React, { useState, useEffect } from "react";
-import { Navbar } from "../components/Navbar";
-import { getDatabase, ref, onValue, off } from "firebase/database";
-import { firestore } from "../services/firebaseConfig";
-import { collection, getDocs, addDoc } from "firebase/firestore";
 
+import { Navbar } from "../components/Navbar";
+
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+  BarChart,
+  Bar,
+} from "recharts";
+import { getDatabase, ref, onValue, off, query } from "firebase/database";
+import { firestore } from "../services/firebaseConfig";
+import { collection, addDoc, doc, getDocs } from "firebase/firestore";
 
 export function Dashboard() {
   const [ledStatus, setLedStatus] = useState(null);
   const [temperatura, setTemperatura] = useState(null);
   const [umidade, setUmidade] = useState(null);
+  const [dataPoints, setDataPoints] = useState([]); 
+
+  async function enviarDadosParaFirebase(ledStatus, temperatura, umidade) {
+    try {
+      const dados = { ledStatus, temperatura, umidade };
+      await addDoc(collection(firestore, "dadosPlantacao"), dados);
+
+      console.log("Dados enviados com sucesso para o Firebase.");
+    } catch (error) {
+      console.error("Erro ao enviar dados para o Firebase:", error);
+    }
+  }
+
+  useEffect(() => {
+    async function fetchFirestoreData() {
+      try {
+        const querySnapshot = await getDocs(
+          collection(firestore, "dadosPlantacao")
+        );
+        const newDataPoints = [];
+
+        querySnapshot.forEach((doc) => {
+          const data = doc.data();
+          if (
+            data.hasOwnProperty("temperatura") &&
+            data.hasOwnProperty("umidade")
+          ) {
+            const { temperatura, umidade } = data;
+            newDataPoints.push({ temperatura, umidade });
+          }
+          const limitDataPoints = newDataPoints.slice(-30)
+          setDataPoints(limitDataPoints);
+        });
+      } catch (error) {
+        console.error("Erro ao buscar dados do Firestore:", error);
+      }
+    }
+
+    fetchFirestoreData();
+  }, []);
 
   useEffect(() => {
     const database = getDatabase();
@@ -19,7 +72,6 @@ export function Dashboard() {
 
     const handleData = (snapshot) => {
       const value = snapshot.val();
-      console.log("Valores recebidos:", value);
 
       const refPath = snapshot.ref.toString();
 
@@ -32,12 +84,10 @@ export function Dashboard() {
       }
     };
 
-    // Registrar listeners para atualizar os estados quando os dados mudarem
     onValue(ledStatusRef, handleData);
     onValue(temperaturaRef, handleData);
     onValue(umidadeRef, handleData);
 
-    // Remover os listeners quando o componente for desmontado
     return () => {
       off(ledStatusRef, handleData);
       off(temperaturaRef, handleData);
@@ -45,17 +95,11 @@ export function Dashboard() {
     };
   }, []);
 
-  async function enviarDadosParaFirebase(ledStatus, temperatura, umidade) {
-    try {
-      console.log('Dados a serem enviados:', { ledStatus, temperatura, umidade });
-      const dados = { ledStatus, temperatura, umidade };
-      await addDoc(collection(firestore, 'dadosPlantacao'), dados);
-  
-      console.log('Dados enviados com sucesso para o Firebase.');
-    } catch (error) {
-      console.error('Erro ao enviar dados para o Firebase:', error);
+  useEffect(() => {
+    if (ledStatus !== null && temperatura !== null && umidade !== null) {
+      enviarDadosParaFirebase(ledStatus, temperatura, umidade);
     }
-  }
+  }, [ledStatus, temperatura, umidade]);
 
   return (
     <div>
@@ -76,8 +120,29 @@ export function Dashboard() {
             <p>{umidade} %</p>
           </div>
         </div>
-        
-        <button className="text-white mt-6 p-2 bg-slate-700" onClick={() => enviarDadosParaFirebase(ledStatus, temperatura, umidade)}>TESTE</button>
+      </div>
+      <div className="flex justify-center">
+      <ResponsiveContainer width="80%" aspect={3}>
+        <LineChart 
+          data={dataPoints} 
+          width={730}
+          height={250}
+          margin={{
+            top: 5,
+            right: 30,
+            left:  20,
+            bottom: 5
+          }}
+          >
+          <CartesianGrid strokeDasharray="1" />
+          <XAxis dataKey="data"/>
+          <YAxis dataKey="temperatura"/>
+          <Tooltip />
+          <Legend />
+          <Line type="natural" dataKey="umidade" stroke="#1ee3cf" activeDot={{ r: 8 }}/>
+          <Line type="natural" dataKey="temperatura" stroke="#6b48ff" />
+        </LineChart>
+      </ResponsiveContainer>
       </div>
     </div>
   );
